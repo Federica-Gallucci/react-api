a7e1089a2de380f054fb2dea4e41070996cbdbff
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-05-28 17:53:59 +0200

    add card for each actresses

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -5,9 +5,10 @@ const apiBaseUrl = `https://lanciweb.github.io/demo/api/actresses/`;
 export default function Main() {
   const [actresses, setActresses] = useState([]);
 
-  useEffect(() => {
+  const fetchActresses = () => {
     axios.get(`${apiBaseUrl}`).then((response) => {
       const results = response.data;
+
       const normalizedActresses = results.map((actress) => {
         const { id, name, birth_year, nationality, biography, image, awards } =
           actress;
@@ -15,33 +16,33 @@ export default function Main() {
         return { id, name, birth_year, nationality, biography, image, awards };
       });
       setActresses(normalizedActresses);
-      console.log(results);
     });
-  }, []);
+  };
+
+  useEffect(fetchActresses, []);
 
   return (
     <main>
       <h1>Lista Attrici</h1>
-      <ul>
+      {/* <p>{JSON.stringify(actresses)}</p> */}
+      <div className="row row-cols-4 gap-3">
         {actresses.map((actress) => {
-          <li key={actress.id}>{actress.name}</li>;
+          return (
+            <div className="container">
+              <div className="card h-100">
+                <div className="card-header">{actress.name}</div>
+                <div className="card-body">
+                  <img src={actress.image} alt={actress.name} />
+                  <p>{actress.birth_year}</p>
+                  <p>{actress.nationality}</p>
+                  <p>{actress.biography}</p>
+                  <p>{actress.awards}</p>
+                </div>
+              </div>
+            </div>
+          );
         })}
-      </ul>
+      </div>
     </main>
   );
 }
-// {
-//     "id": 3,
-//     "name": "Audrey Hepburn",
-//     "birth_year": 1929,
-//     "death_year": 1993,
-//     "nationality": "British",
-//     "most_famous_movies": [
-//         "Breakfast at Tiffany's",
-//         "Roman Holiday",
-//         "My Fair Lady"
-//     ],
-//     "awards": "1 Academy Award, 3 Golden Globe Awards",
-//     "biography": "Audrey Hepburn was a beloved actress and fashion icon, known for her elegance and timeless performances.",
-//     "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Audrey_Hepburn_1956.jpg/220px-Audrey_Hepburn_1956.jpg"
-// }
